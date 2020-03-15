78cb7709beb161de322c70e31732ab9e1c24b080
shreesh <shreesharya20@gmail.com>
2020-03-16 02:49:53 +0530

    admin changes

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import psycopg2.extensions
+# import psycopg2.extensions
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'btre/static')
     ]
+
+# media folder settings
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
